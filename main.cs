96084d9922ac21b226eebb18b78deaf100f3e7a4
Zachariah Townsend <>
2021-04-20 17:21:54 +0000

    Final clean up, added in comments to explain the methods, package up everything nicely

--- main.cs
+++ main.cs
@@ -27,9 +27,9 @@ class MainClass {
     UpdateScreen(breakUpdate);
   }
 
-  // UpdateScreen is a loop that displays the prompt and room description, then asks for the player to input something.
-  // Whatever the player inputs is handled, which could potentially be a lot of tasks, before the code finally returns to UpdateScreen();
-  // Once the code comes back to UpdateScreen, the output of the player's actions are displayed, and UpdateScreen calls itself
+    // UpdateScreen is a loop that displays the prompt and room description, then asks for the player to input something.
+    // Whatever the player inputs is handled, which could potentially be a lot of tasks, before the code finally returns to UpdateScreen();
+    // Once the code comes back to UpdateScreen, the output of the player's actions are displayed, and UpdateScreen calls itself
   static void UpdateScreen(bool breakLoop) {
     if(breakLoop == false) {
       DisplayPrompt(skipPrompt);
@@ -39,6 +39,8 @@ class MainClass {
     }
   }
 
+    // RegisterInstances() moves all of the clutter involved with 
+    // constructing different instances of rooms and stuff out of Main(), for clarity
   static void RegisterInstances() {
     // Rooms
       // Cellar0
@@ -48,7 +50,6 @@ class MainClass {
       string decRmID = "Cellar0";
       bool decCurm = false;
       Cellar0 = new Room(decExts.Split(", "), decItms.Split(", "), decDesc, decRmID, decCurm);
-      Cellar0.Start();
 
       Room.roomList = new string[Room.roomCount];
 
@@ -57,11 +58,11 @@ class MainClass {
     Player.SetCurrentRoom();
   }
 
-  // DisplayPrompt writes a prompt
-  // It can optionally be skipped to avoid console clutter
+    // DisplayPrompt writes a prompt
+    // It can optionally be skipped to avoid console clutter
   static void DisplayPrompt(bool skip) {
     if(skip == false) {
-      Util.Write(MainClass.Cellar0.DescribeRoom());
+      Util.Write(Player.currentRoom.DescribeRoom());
       Util.Write("What will you do next?");
       Util.Write("Enter 'Help' for a list of options");
     }
@@ -97,17 +98,20 @@ public class InputProcessing {
   }
 
   // Input behaviour methods 
-  // These methods are called in a switch block in ProcessInput
-  // Each method must return a string
+  // These methods are called in a switch block in ProcessInput and each must return a string
+    // Writes out all of the player's current options
   public static string WriteOptions() {
     return "Your options are: " + Util.WriteArray(InputOptions, true, false);
   }
+    // Reveals the jewelry box
   public static string MoveDrapes() {
     return "You found a jewelry box!" + AddInputOption("Open Jewelry Box");
   }
+    // Shows text for opening the box and calls CueEndScreen()
   public static string OpenJewelryBox() {
     return "You found a pearl necklace" + CueEndScreen();
   }
+    // Shows end text
   public static string CueEndScreen() {
     string msg = Util.newLine + Util.lineBreak + Util.newLine + "Congratulations, you found the treasure!" + Util.newLine + 
            "This is a prototype for a text adventure written in C#" + Util.newLine +
@@ -117,6 +121,7 @@ public class InputProcessing {
   }
 
   // Inspect action
+    // Asks the player which item they want to inspect, makes sure that item is available to be inspected, and then hands over control to Inspect()
   public static string BeginInspectLoop(string[] items) {
     string returnString = "";
     Util.Write("What will you inspect?");
@@ -128,6 +133,8 @@ public class InputProcessing {
     string input = Console.ReadLine();
     return Inspect(input, items);
   }
+
+    // Takes in the item which the player has asked to inspect and processes + returns that information
   public static string Inspect(string input, string[] items) {
     if(Util.CheckForError(input, items) == true) {
       return "Not an input";
@@ -141,6 +148,7 @@ public class InputProcessing {
   }
 
   // Input utils
+    // Adds an input option to the list of options, and tells the player that the option has been added 
   public static string AddInputOption(string option) {
     for(int i = 0; i < InputOptions.Length; i++ ) {
       if(InputOptions[i].Length == 0) {
@@ -150,6 +158,8 @@ public class InputProcessing {
     }
     return null;
   }
+
+    // Removes an input option from the list of options
   public static void RemoveInputOption(string option) {
     for(int i = 0; i < InputOptions.Length; i++ ) {
       if(InputOptions[i] == option) {
@@ -170,15 +180,15 @@ public class Room {
   public string RoomID;
   public bool currentRoom;
 
+    // Returns the room's description
   public string DescribeRoom() {
-    return this.description;
+    return description;
   }
 
-  public void Start() {
-    roomCount++;
-  }
-
+    // Instance constructor
+    // Sets instance variables and also increments the total amount of rooms
   public Room(string[] xts, string[] itm, string dsc, string rid, bool cur) {
+    roomCount++;
     exits = xts;
     items = itm;
     description = dsc;
@@ -187,39 +197,49 @@ public class Room {
   }
 }
 
-// The player class is instantiated once and tracks where the player is,
+// The player class is instantiated once and will track where the player is,
 // what's in their inventory, etc
 public class EntityPlayer {
   public Room currentRoom;
 
+    // Instance constructor
   public EntityPlayer() {
   }
 
+    // Returns the description field of the room the player is currently in 
   public string DescribeCurrentRoom() {
     return currentRoom.description;
   }
 
+    // Sets the current room
+    // Since the only room in the game right now is Cellar0, this is hardcoded for simplicity
   public void SetCurrentRoom() {
     currentRoom = MainClass.Cellar0;
   }
 }
 
-// Util is a toolbox with some nice functions, mostly for more Console control
-// 
+// Util is a toolbox with some nice functions, mostly for more array and console writing control
 public static class Util {
   public static string newLine = "\r\n";
   public static string indent = "    ";
   public static string lineBreak = "--------------------------------";
   
   // Text utils
+    // Glorified Console.Write for more control over what gets written
+    // Also abstracts away formatting: instead of Console.Write-ing everying with the text passed thru FormatText,
+    // this method does it for me
   public static void Write(string text) {
     Console.Write(FormatText(text));
   }
 
+    // Takes a block of text and formats it with an indent, new line, and a horizontal line above it
   private static string FormatText(string text) {
     return indent + text + newLine + lineBreak + newLine;
   }
 
+    // Takes an array, traverses the contents, and writes each
+    // ignoreEmpty (bool): whether to write out empty entries from the array
+    // format (bool): whether to apply formatting (indents, newlines, etc)
   public static string WriteArray(string[] array, bool ignoreEmpty, bool format) {
     string returnString = "";
     for(var i = 0; i < array.Length; i++ ) {
@@ -236,27 +256,10 @@ public static class Util {
       returnString = returnString.Substring(0, returnString.Length-2);
     }
     return returnString;
-  } 
-
-  public static string WriteArray(int[] array, bool ignoreEmpty, bool format) {
-    string returnString = "";
-    for(var i = 0; i < array.Length; i++ ) {
-      if(array[i].ToString().Length == 0 && ignoreEmpty == true) {
-        continue;
-      } else {
-        returnString += array[i].ToString();
-        if(i != array.Length - 1) {
-          returnString += ", ";
-        }
-      }
-    }
-    if(returnString.Substring(returnString.Length-2) == ", ") {
-      returnString = returnString.Substring(0, returnString.Length-2);
-    }
-    return returnString;
   }
 
   // Variable utils
+    // Checks if a selected option (string check) is in an array (string[] array)
   public static bool CheckForError(string check, string[] array) {
     for(int i = 0; i < array.Length; i++){
       if(check == array[i]){
@@ -265,7 +268,7 @@ public static class Util {
     }
     return true;
   }
-
+    // Toggles a boolean
   public static bool ToggleBool(bool toggle) {
     if(toggle) {
       return false;
